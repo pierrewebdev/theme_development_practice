2521eeaedf438f9720f59806b1db0da549848091
pierrewebdev <patrick.pierre000@gmail.com>
2020-12-16 19:47:22 -0500

    changed the footer.php to remove wordpress footer links

---
+++ wp-content/themes/starter/footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package starter
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a style = "color:black; text-decoration:none;" href="<?php echo esc_url( __( "https://github.com/pierrewebdev" ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				  echo "Powered By Pierre Web Development";
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				  echo "Awesome Practice Theme";
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/starter/front-page.php
@@ -0,0 +1,32 @@
+<?php 
+  get_header();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practice Theme</title>
+</head>
+<body>
+
+<main id="primary" class="site-main">
+
+<?php
+while ( have_posts() ) :
+    the_post();
+
+    get_template_part( 'template-parts/content', 'page' );
+
+endwhile; // End of the loop.
+?>
+
+</main><!-- #main -->
+
+</body>
+</html>
+
+<?php
+get_footer();
+?>
